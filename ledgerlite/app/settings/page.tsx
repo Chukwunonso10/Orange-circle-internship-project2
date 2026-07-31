51143fd64e9c6c953106323a5f34f8df5deb9799
EraTech <raymondedosa400@gmail.com>
2026-07-31 06:38:34 -0700

    added settings UI, home and mobile

--- ledgerlite/app/settings/page.tsx
+++ ledgerlite/app/settings/page.tsx
@@ -1,5 +1,7 @@
 import SideNav from "@/components/sideNav";
 import UserNav from "@/components/userNav";
+import LedgerLiteSettings from "@/components/settings";
+
 
 export default function Settings() {
   return (
@@ -12,15 +14,8 @@ export default function Settings() {
           <UserNav />
         </div>
         <main className="ml-10 md:ml-72 sm:ml-10  p-6">
-          <div className="space-y-6">
-            <div>
-              <h1 className="text-3xl font-bold text-slate-900 dark:text-white">
-                Settings Page
-              </h1>
-              <p className="mt-2 text-slate-600 dark:text-slate-400">
-                This is the dashboard page. You can manage your dashboard here.
-              </p>
-            </div>
+          <div className="">
+           <LedgerLiteSettings />
           </div>
         </main>
       </div>
